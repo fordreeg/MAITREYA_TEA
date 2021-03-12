31bdd0aa8f9c2d1571d25c58dfef120a380fac16
Vlad <fordreef@gmail.com>
2021-03-12 23:38:27 +0200

    add consultation section and php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -70,4 +70,54 @@ $(document).ready(function(){
         .addClass('catalog-header__btn_active').siblings().removeClass('catalog-header__btn_active')
         .closest('div.container').find('div.catalog__wrapper').removeClass('catalog__wrapper_active').eq($(this).index()).addClass('catalog__wrapper_active');
     });
+
+    // Модальное окно
+    $('[data-modal=thanks]').on('click', function() {
+        $('.overlay, #thanks').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    });
+
+    // Валидация формы
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+
+    // Маска для номера тлф
+    $('input[name=phone]').mask("+380 (99) 999-99-99");
+
+    // PHP mailer
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
